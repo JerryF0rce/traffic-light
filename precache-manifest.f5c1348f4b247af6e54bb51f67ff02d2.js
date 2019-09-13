9f51689258a9c277919d40cf9903d7d9ab49ec06
T-rex <john1kote@gmail.com>
2019-09-13 23:23:12 +0300

    Updates

---
+++ precache-manifest.f5c1348f4b247af6e54bb51f67ff02d2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b04615b805f7fe2cd0a84c23d7c9e3a",
+    "url": "/traffic-light/index.html"
+  },
+  {
+    "revision": "916a212d064b1310b3c4",
+    "url": "/traffic-light/static/css/main.04848d5b.chunk.css"
+  },
+  {
+    "revision": "b7ae42cfc608a204d440",
+    "url": "/traffic-light/static/js/2.226a5f63.chunk.js"
+  },
+  {
+    "revision": "916a212d064b1310b3c4",
+    "url": "/traffic-light/static/js/main.3e4c225c.chunk.js"
+  },
+  {
+    "revision": "51d9b3fb16def4a0c644",
+    "url": "/traffic-light/static/js/runtime~main.ba433894.js"
+  }
+]);
