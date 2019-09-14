4e2f40042fc85f547e062d98e428e9a3ac287f7d
T-rex <john1kote@gmail.com>
2019-09-14 12:04:35 +0300

    upd routes

--- src/components/Navigation/MainMenu.js
+++ src/components/Navigation/MainMenu.js
@@ -5,9 +5,9 @@ const MainMenu = () => {
    return (
       <header>
          <div className="container">
-            <a href="/red-light">Go to: /red-light</a>
-            <a href="/yellow-light">Go to: /yellow-light</a>
-            <a href="/green-light">Go to: /green-light</a>
+            <a href="/#/red-light" >Go to: /red-light</a>
+            <a href="/#/yellow-light" >Go to: /yellow-light</a>
+            <a href="/#/green-light" >Go to: /green-light</a>
          </div>
       </header>
    );
--- src/components/UI/trafficLight.js
+++ src/components/UI/trafficLight.js
@@ -5,6 +5,7 @@ class TrafficLight extends Component {
    constructor(props) {
       super(props);
       this.state = {
+         currentLightRoute: this.props.history.location.pathname,
          redLight: {
             duration: 10000,
             blinkingDuration: 3000,
@@ -16,26 +17,32 @@ class TrafficLight extends Component {
             duration: 15000,
             blinkingDuration: 3000,
          },
-         lastLight: sessionStorage.getItem('lastLight') || ''
+         lastLight: sessionStorage.getItem('lastLight') || '',
+         lastTimerID: sessionStorage.getItem('lastTimerID') || ''
       };
    }
 
-   componentDidMount() {
-      const currentLightRoute = this.props.history.location.pathname;
-      const currentLight = this.transformRoute(currentLightRoute);
+   componentDidMount () {
+      const currentLight = this.transformRoute(this.state.currentLightRoute);
+
       const currentLightElement = document.querySelector(`.${currentLight}`);
       // before '/yellow-light' after 'yellowLight'
 
-      if (currentLight !== 'yellowLight')sessionStorage.setItem('lastLight', currentLight);
+      if (currentLight !== 'yellowLight') {
+         sessionStorage.setItem('lastLight', currentLight);
+         this.numberUntilNextLight(currentLight, currentLightElement)
+      }
 
-      if (currentLight === 'redLight' || 'greenLight') this.goToYellowLight(currentLight, currentLightElement);
+      if (currentLight === 'redLight' || 'greenLight') {
+         this.goToYellowLight(currentLight);
+      }
       if (currentLight === 'yellowLight' && !this.state.lastLight) this.goToRandomLight(currentLight);
       if (currentLight === 'yellowLight' && this.state.lastLight === 'greenLight') this.goToRedLight(currentLight);
       if (currentLight === 'yellowLight' && this.state.lastLight === 'redLight') this.goToGreenLight(currentLight);
 
       currentLightElement.classList.add('activeLights');
-
    }
+
 
    transformRoute = (curLight) => {
       return curLight
@@ -44,54 +51,55 @@ class TrafficLight extends Component {
             (word, i) => i === 0 ?  word.slice(1) : word[0].toUpperCase() + word.slice(1)
          )
          .join('');
-}
+   }
 
-   goToYellowLight = (currentLight , currentLightElement) => {
+   setTimer = (path, duration) => {
+      let storageTimerId = +this.state.lastTimerID;
+      clearTimeout(storageTimerId);
+      let timerID = setTimeout(() => {
+         if (path !== '/yellow-light') {
+         sessionStorage.setItem('lastLight', `${this.transformRoute(path)}`);
+         }
+         this.props.history.replace(path);
+      }, duration);
+      sessionStorage.setItem('lastTimerID', `${timerID}`)
+   }
+
+   numberUntilNextLight = (currentLight , currentLightElement) => {
       let totalSeconds = this.state[currentLight].duration / 1000;
-      let showTimer = null;
-      let timerId = null;
-      if (currentLight !== 'yellowLight') {
-         showTimer = document.querySelector(`.${currentLight}`).children[0];
-         showTimer.textContent = totalSeconds - 1;
-         timerId = setInterval(() => {
-            showTimer.textContent = totalSeconds - 2
-            totalSeconds--
-            if (totalSeconds === 4) {
-               currentLightElement.classList.add('hightlight')
-            }
-            if (totalSeconds === 0) {
-               currentLightElement.classList.remove('hightlight')
-            }
-         }, 1000);
-      }
-
-      setTimeout(() => {
-         clearInterval(timerId);
-         this.props.history.replace('/yellow-light');
-      }, this.state[currentLight].duration);
+      let showTimer = document.querySelector(`.${currentLight}`).children[0];
+      showTimer.textContent = totalSeconds - 1;
+      let timerId = setInterval(() => {
+         showTimer.textContent = totalSeconds - 2
+         totalSeconds--
+         if (totalSeconds === 4) {
+            currentLightElement.classList.add('hightlight')
+         }
+         if (totalSeconds === 0) {
+            clearInterval(timerId);
+            currentLightElement.classList.remove('hightlight')
+         }
+      }, 1000);
    }
 
    goToRandomLight = (currentLight) => {
       const lightsRoutes = ['/green-light', '/red-light'];
       let randomCount = Math.floor(Math.random()*2);
       sessionStorage.setItem('lastLight', this.transformRoute(lightsRoutes[randomCount]));
-      setTimeout(() => {
-         this.props.history.replace(lightsRoutes[randomCount]);
-      }, this.state[currentLight].duration);
+      this.setTimer(lightsRoutes[randomCount], this.state[currentLight].duration)
+   }
+
+   goToYellowLight = (currentLight) => {
+      if (this.state.currentLightRoute === '/yellow-light') return;
+      this.setTimer('/yellow-light', this.state[currentLight].duration);
    }
 
    goToRedLight = (currentLight) => {
-      setTimeout(() => {
-         sessionStorage.setItem('lastLight', 'redLight');
-         this.props.history.replace('/red-light');
-      }, this.state[currentLight].duration);
+      this.setTimer('/red-light', this.state[currentLight].duration);
    }
 
    goToGreenLight = (currentLight) => {
-      setTimeout(() => {
-         sessionStorage.setItem('lastLight', 'greenLight');
-         this.props.history.replace('/green-light');
-      }, this.state[currentLight].duration);
+      this.setTimer('/green-light', this.state[currentLight].duration);
    }
 
    render () {
